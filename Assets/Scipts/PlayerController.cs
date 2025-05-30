ea724baddf345fdad0e620e5bdae2fe029fdf50f
trsilwi02 <anh.td235009@sis.hust.edu.vn>
2025-05-30 18:41:05 +0700

    Add .gitignore for Unity

---
+++ Assets/Scipts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    private Animator animator;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+        UpdateAnimation();
+    }
+
+    private void HandleMovement()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y); // Di chuyển ngang
+        if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1); // đi sang phải thì quay sang phải 
+        }
+        else if (moveInput < 0) {
+            transform.localScale = new Vector3 (-1,1,1); //  đi sang trái thì quay sang trái
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2 (rb.linearVelocity.x, jumpForce);
+        }
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    private void UpdateAnimation()
+    {
+        bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
+        bool isJumping = !isGrounded;
+        animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isJumping", isJumping);
+    }
+}
